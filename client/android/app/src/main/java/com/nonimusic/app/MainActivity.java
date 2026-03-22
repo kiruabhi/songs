cbf4a812db9f90800ef3ca65d2590d3c732837ac
kiruabhi <vishnu212v@gmail.com>
2026-03-22 14:17:29 +0530

    feat: setup Capacitor Android project with runtime background process plugin

---
+++ client/android/app/src/main/java/com/nonimusic/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nonimusic.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Enable cordova background mode if in a Capacitor/Cordova native container
+document.addEventListener('deviceready', () => {
+  const cordova = (window as any).cordova;
+  if (cordova?.plugins?.backgroundMode) {
+    cordova.plugins.backgroundMode.enable();
+    cordova.plugins.backgroundMode.on('activate', () => {
+       cordova.plugins.backgroundMode.disableWebViewOptimizations();
+    });
+  }
+}, false);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
